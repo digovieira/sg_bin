ce6e9c843e2bfa13aeb5e5c697dcf126cc69cca3
root <root@proxy.otaciliocosta.local>
2025-10-22 11:54:05 -0300

    Remove arquivos idênticos à main

--- dhcp_reload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo /usr/local/bin/dhcpgen /etc/dhcp/
-sudo /usr/local/bin/firewallgen /var/www/sistemas/bin/ 
-sudo /var/www/sistemas/bin/sgipset.range
-sudo systemctl restart dhcpd
-echo Regras ativadas com sucesso!
-exit 0
-
--- squid_reload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo systemctl restart squid.service
-echo Regras ativadas com sucesso!
-exit 0
-
