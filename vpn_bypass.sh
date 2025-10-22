a0efbe16b7c669f60b90fb90e8fd1532b54d5068
root <root@proxy.aeroar.com.br>
2025-10-22 08:32:09 -0300

    Adicionados arquivos novos da branch aeroar

---
+++ vpn_bypass.sh
@@ -0,0 +1,8 @@
+####
+# Arquivo de bypass automaticamente criado.
+# Criado em 12-28-2015, 10:26:11
+#
+# Importante! Nao edite diretamente este arquivo pois as mudancas podem ser sobrescritas!
+#
+####
+
