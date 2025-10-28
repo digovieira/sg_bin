61ecf44d5724b3c3e0e73d8eb7acbff296ceedaf
root <root@proxy.lsagencia.local>
2025-10-28 20:00:23 -0200

    Remove arquivos idênticos à main

--- check_ping.sh
+++
@@ -1,2 +0,0 @@
-# !/bin/sh
-/usr/lib64/nagios/plugins/check_ping -H $1 -w 10,20% -c 100,50% -p 1 | awk '{print $2}'
--- squid-mkswap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/mkdir -p /var/run/squid
-/usr/bin/chown  squid.squid  /var/run/squid
-/usr/libexec/squid/cache_swap.sh
